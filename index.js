029de7c27fc6501f70e5da3933d2af8c5d6e6d61
AndresBarraganM <al22760549@ite.edu.mx>
2025-03-19 15:32:05 -0700

    Archivos base creados

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+//Creacion de la App
+const createApp = () => {
+  const app = express();
+
+  app.use(json());
+  app.disable('x-powered-by');
+
+  const PORT = process.env.PORT || 1234;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+}
