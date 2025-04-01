3003f336424fdf8655a129d1f204399d63cf4300
AndresBarraganM <al22760549@ite.edu.mx>
2025-04-01 12:58:40 -0700

    Alterado el controlador de incidentes

--- Controlers/ControlerIncidentes.js
+++ Controlers/ControlerIncidentes.js
@@ -7,21 +7,34 @@ export class ControlerIncidentes {
   // retornar incidentes
   static async getAll(req, res) {
     try {
-      const incidentes = await Incidente.getIncidentes();
-      res.status(200).json(incidentes);
-    } catch (error) {
-      res.status(500).json(error);
+      const filtros = {};
+
+      // Extrae los parámetros de la query y los agrega a los filtros si existen
+      if (req.query.edificio) filtros.edificio = req.query.edificio;
+      if (req.query.banio) filtros.banio = req.query.banio;
+      if (req.query.planta) filtros.planta = req.query.planta;
+      if (req.query.fechaAntesDe || req.query.fechaDespuesDe) {
+        filtros.fecha = {};
+        if (req.query.fechaAntesDe) filtros.fecha.$lte = new Date(req.query.fechaAntesDe);
+        if (req.query.fechaDespuesDe) filtros.fecha.$gte = new Date(req.query.fechaDespuesDe);
+      }
+      
+      // recuperar reporte
+      const incidentes = "NO IMPLEMENTADO";
+
+      res.json(incidentes);
+    }catch(error){
+      res.status(500).json({ message: 'Error al recuperar los incidentes', error });
     }
   }
 
-  static async getByParams(req, res) {
-    parametros = req.params;
-    console.log(parametros);
-
-    return
-  }
-
   static async getFotoIncidente(req, res) {
+    const id = req.params.id;
+
+    if (!id){
+      res.status(400).json({ message: 'Se requiere de una id' });
+      return
+    }
     return
   }
 
