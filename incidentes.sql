766c700cc6edee4946947f959a30bcf89d34c84d
diego <eldieguillo87@gmail.com>
2025-04-02 02:55:03 -0700

    backup de la base de datos

---
+++ incidentes.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 02, 2025 at 02:53 AM
+-- Server version: 11.5.2-MariaDB
+-- PHP Version: 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `incidentes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `historial_incidentes`
+--
+
+DROP TABLE IF EXISTS `historial_incidentes`;
+CREATE TABLE IF NOT EXISTS `historial_incidentes` (
+  `id_historial` int(11) NOT NULL AUTO_INCREMENT,
+  `id_incidente` int(11) NOT NULL,
+  `id_usuario_responsable` int(11) DEFAULT NULL,
+  `accion` varchar(50) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `fecha_registro` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_historial`),
+  KEY `fk_incidentes` (`id_incidente`),
+  KEY `fk_usuarios_responsable` (`id_usuario_responsable`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incidentes`
+--
+
+DROP TABLE IF EXISTS `incidentes`;
+CREATE TABLE IF NOT EXISTS `incidentes` (
+  `id_incidente` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario_reporte` int(11) NOT NULL,
+  `edificio` enum('100','200','300','400','500','600','700','centro_informacion','gimnacio','auditorio') NOT NULL,
+  `planta` enum('alta','baja') NOT NULL,
+  `banio` enum('hombres','mujeres') NOT NULL,
+  `descripcion` text NOT NULL,
+  `img` mediumblob NOT NULL,
+  `fecha_reporte` datetime NOT NULL,
+  `estado` enum('pendiente','en_proceso','resuelto') NOT NULL DEFAULT 'pendiente',
+  `prioridad` enum('baja','media','alta') NOT NULL,
+  PRIMARY KEY (`id_incidente`),
+  KEY `fk_usuarios` (`id_usuario_reporte`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Dumping data for table `incidentes`
+--
+
+INSERT INTO `incidentes` (`id_incidente`, `id_usuario_reporte`, `edificio`, `planta`, `banio`, `descripcion`, `img`, `fecha_reporte`, `estado`, `prioridad`) VALUES
+(7, 5, '300', 'alta', 'mujeres', 'Fuga de agua en el baño de mujeres', 0x3130313130313130313031, '2025-04-02 02:26:03', 'pendiente', 'media'),
+(8, 5, '300', 'alta', 'mujeres', 'Fuga de agua en el baño de mujeres', 0x3130313130313130313031, '2025-04-02 02:26:45', 'pendiente', 'media'),
+(10, 5, '300', 'alta', 'mujeres', 'Fuga de agua en el baño de mujeres', 0x3130313130313130313031, '2025-04-02 02:32:19', 'pendiente', 'media'),
+(11, 5, '300', 'alta', 'mujeres', 'Fuga de agua en el baño de mujeres', 0x3130313130313130313031, '2025-04-02 02:34:38', 'pendiente', 'media'),
+(12, 5, '300', 'alta', 'mujeres', 'Fuga de agua en el baño de mujeres', 0x3130313130313130313031, '2025-04-02 02:34:54', 'pendiente', 'media'),
+(13, 5, '300', 'alta', 'mujeres', 'Fuga de agua en el baño de mujeres', 0x3130313130313130313031, '2025-04-02 02:35:06', 'pendiente', 'media');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notificaciones`
+--
+
+DROP TABLE IF EXISTS `notificaciones`;
+CREATE TABLE IF NOT EXISTS `notificaciones` (
+  `id_notificacion` int(11) NOT NULL AUTO_INCREMENT,
+  `id_incidente` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `tipo_notificacion` enum('SMS','email','app') NOT NULL,
+  `mensaje` text NOT NULL,
+  `fecha_envio` datetime DEFAULT NULL,
+  `estado` enum('pendiente','enviado','fallido') DEFAULT NULL,
+  PRIMARY KEY (`id_notificacion`),
+  KEY `fk_usuario` (`id_usuario`),
+  KEY `fk_incidente` (`id_incidente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `rol` enum('administrador','encargado_limpieza') NOT NULL,
+  `contrasenia` varchar(255) NOT NULL,
+  `estado` enum('activo','inactivo') DEFAULT 'activo',
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre`, `email`, `telefono`, `rol`, `contrasenia`, `estado`) VALUES
+(4, 'Diego Francisco Soto Flores', 'eldieguillo87@gmail.com', '12345678', 'encargado_limpieza', 'pizzaconpiña', 'inactivo'),
+(5, 'Salva Laguna', 'al22760575@ite.edu.mx', '5849265365', 'encargado_limpieza', 'atunconarroz', 'activo');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `historial_incidentes`
+--
+ALTER TABLE `historial_incidentes`
+  ADD CONSTRAINT `fk_incidentes` FOREIGN KEY (`id_incidente`) REFERENCES `incidentes` (`id_incidente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_usuarios_responsable` FOREIGN KEY (`id_usuario_responsable`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `incidentes`
+--
+ALTER TABLE `incidentes`
+  ADD CONSTRAINT `fk_usuarios` FOREIGN KEY (`id_usuario_reporte`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `notificaciones`
+--
+ALTER TABLE `notificaciones`
+  ADD CONSTRAINT `fk_incidente` FOREIGN KEY (`id_incidente`) REFERENCES `incidentes` (`id_incidente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
