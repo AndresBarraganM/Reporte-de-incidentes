259eb28465577170c1657ed736fcc99efbe4836d
AndresBarraganM <al22760549@ite.edu.mx>
2025-04-10 17:18:38 -0700

    schema tipoIncidente creado

---
+++ Schemas/TipoIncidenteSchema.js
@@ -0,0 +1,26 @@
+import z from 'zod';
+import { TipoIncidenteModel } from '../Modelos/AmazonRDS/TipoIncidenteModel';
+
+const schemaTipoincidente = z.object({
+  Tipo_incidente: z.string().max(250)
+  .refinement(async(tipo) => {
+    return await revisarQueExisteEnDatabase(tipo)
+  }, {
+    message: 'El tipo de incidente no existe en la base de datos',
+  })
+})
+
+async function revisarQueExisteEnDatabase(tipo) {
+  const existe = await getTipoIncidente(incidente)
+  if (existe == null) {
+    return false
+  } else {
+    return true
+  }
+}
+
+export function validarIncidenteZod(incidente){
+  const resultado = schemaTipoincidente.safeParse(incidente)
+  
+  return resultado
+}
