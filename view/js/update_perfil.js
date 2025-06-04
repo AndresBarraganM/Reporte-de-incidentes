f798310a84cb4e921f9abe83db53bc2b1127286d
AndresBarraganM <al22760549@ite.edu.mx>
2025-06-03 20:31:22 -0700

    validaciones basicas a actualizar perfil

--- view/js/update_perfil.js
+++ view/js/update_perfil.js
@@ -7,6 +7,8 @@ async function updatePerfil(nombre, contrasena, correo, telefono) {
     if (contrasena) datosActualizados.contrasena_hash = contrasena;
     if (correo) datosActualizados.email = correo;
     if (telefono) datosActualizados.telefono = telefono;
+
+    // ----------------------VERIFICACIÓN DE DATOS----------------------
     // Verifica si hay datos para actualizar
     if (Object.keys(datosActualizados).length === 0) {
       Swal.fire({
@@ -14,10 +16,33 @@ async function updatePerfil(nombre, contrasena, correo, telefono) {
       title: 'Sin cambios',
       text: 'No se han realizado cambios para actualizar.',
       confirmButtonColor: '#3085d6'
-});
+      });
+      return;
+    }
+    // verifica que el telefono tenga 10 dígitos y sea numérico
+    if (telefono && (!/^\d{10}$/.test(telefono))) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Teléfono inválido',
+        text: 'El número de teléfono debe tener 10 dígitos numéricos.',
+        confirmButtonColor: '#d33'
+      });
       return;
     }
 
+    // verifica que el correo tenga un formato válido
+    if (correo && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(correo)) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Correo inválido',
+        text: 'El correo electrónico no tiene un formato válido.',
+        confirmButtonColor: '#d33'
+      });
+      return;
+    }
+
+
+    // ----------------------ENVIO DE DATOS----------------------
     // Realiza la solicitud PATCH al endpoint correspondiente
     const response = await fetch('http://localhost:1234/usuario/actualizar', {
       method: 'PATCH',
