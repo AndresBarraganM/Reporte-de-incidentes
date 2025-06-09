02ff489e5146c6b6772c909101eec6cf779bc9b0
AndresBarraganM <al22760549@ite.edu.mx>
2025-06-09 12:54:40 -0700

    la direccion ip esta almacenada en un solo lugar

---
+++ view/js/setConfiguracionServidor.js
@@ -0,0 +1,6 @@
+// este script define la URL del servidor
+// y la expone como una variable global para que pueda ser utilizada en otras partes de la aplicación.
+
+window.serverConfig = {
+    direccionServidor: 'http://10.4.25.37/', // Cambia esto a la URL de tu servidor
+    };
