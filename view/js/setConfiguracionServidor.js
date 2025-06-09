30fa91df38cb4a674d7c9e34a7104097bae823e3
AndresBarraganM <al22760549@ite.edu.mx>
2025-06-09 13:10:15 -0700

    direccion de la API alterada

--- view/js/setConfiguracionServidor.js
+++ view/js/setConfiguracionServidor.js
@@ -2,5 +2,5 @@
 // y la expone como una variable global para que pueda ser utilizada en otras partes de la aplicación.
 
 window.serverConfig = {
-    direccionServidor: 'http://10.4.25.37/', // Cambia esto a la URL de tu servidor
+    direccionServidor: 'http://10.4.25.37:1234/', // Cambia esto a la URL de tu servidor
     };
